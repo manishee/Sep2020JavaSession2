e4fe15a02cecf863f9bb801680b52c5e10aeeea0
Manish Chugh <manishchugh1981@gmail.com>
2020-10-07 17:04:24 -0400

    code from oct 7

---
+++ src/SeleniumSessions/ArrayListConcept.java
@@ -0,0 +1,89 @@
+package SeleniumSessions;
+
+import java.util.ArrayList;
+
+public class ArrayListConcept {
+
+	public static void main(String[] args) {
+			
+		ArrayList ar=new ArrayList(); // default class in java
+		
+		System.out.println(ar.size());//size is printing the physical capacity
+		ar.add(100);//0
+		ar.add(200);//1
+		
+		System.out.println(ar.size());
+		ar.add(300);//2
+		ar.add(400);//3
+		
+		System.out.println(ar.size());
+		ar.add(500);//4
+		ar.add(600);//5
+		ar.add(700);//6
+		//System.out.println(ar.get(7));java.lang.IndexOutOfBoundsException: Index 7 out of bounds for length 7
+		
+		System.out.println(ar.size());
+		ar.remove(6);
+		ar.add(800);//7
+		ar.add(900);//8
+		ar.add(1000);//9
+		ar.add(1100);//10
+		
+		System.out.println(ar.get(5));
+		
+		for(int i=0; i<ar.size(); i++) {
+			System.out.println(ar.get(i));
+		}
+		
+		//Dynamic ArrayList with different data
+		System.out.println("------------");
+		ArrayList arr=new ArrayList();
+		arr.add("Tom");
+		arr.add('m');
+		arr.add(38);
+		
+		for(int j=0; j<arr.size(); j++) {
+			System.out.println(arr.get(j));
+		}
+		
+		//generics
+		System.out.println("----------");
+		ArrayList<Integer> marksList=new ArrayList<Integer>();
+		marksList.add(80);
+		marksList.add(85);
+		marksList.add(88);
+		//marksList.add("Tom");
+		//for each loop
+		for(Integer ele : marksList) {
+			System.out.println(ele);
+		}
+		System.out.println("-----------");
+		
+		ArrayList<String> countryList=new ArrayList<String>();
+		countryList.add("USA");
+		countryList.add("India");
+		//for each loop
+		for(String data : countryList) {
+			System.out.println(data);
+		}
+		System.out.println("-----------");
+		
+		//empData --> empName, empAge, empGender, IsPerm, 
+		ArrayList<Object> empData=new ArrayList<Object>();
+		empData.add("Tom");
+		empData.add(25);
+		empData.add('m');
+		empData.add(true);
+		empData.add(34.55);
+		
+		for(int k=0; k<empData.size(); k++) {
+			System.out.println(empData.get(k));
+		}
+		System.out.println("-----------");
+		//for each loop
+		for(Object data : empData) {
+			System.out.println(data);
+		}
+	}
+
+}
---
+++ src/SeleniumSessions/Car.java
@@ -0,0 +1,41 @@
+//************ Oct 7 *************
+package SeleniumSessions;
+
+public class Car { // template
+	
+	//global variables or class variables
+	String name;
+	String color;
+	int price;
+	String model;
+
+	public static void main(String[] args) {
+		
+		//create an object of class : using new keyword
+		Car c1=new Car(); // new Car() -> object, c1 is the name /reference
+		//Car is the class
+		c1.color="Blue";
+		c1.name="BMW";
+		c1.price=40000;
+		c1.model="520d";
+		
+		Car c2=new Car();
+		c2.color="White";
+		c2.name="Audi";
+		c2.price=30000;
+		c2.model="Q5";
+		
+		System.out.println(c1.name +" "+c1.color +" " +c1.price +" "+c1.model);
+		System.out.println(c2.name +" "+c2.color +" " +c2.price +" "+c2.model);
+		// no reference object
+		//new Car().name="Honda";
+		//new Car().model="Civic";
+		
+		//Null reference object
+		Car c3=new Car();
+		c3=null;
+		
+		System.gc();
+	}
+
+}
